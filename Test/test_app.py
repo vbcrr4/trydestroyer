0072acdcd18a174b0b84edf75798ecc85d049d1c
v.becerra <v.becerra@alumno.um.edu.ar>
2023-09-27 09:02:57 -0300

    update

--- Test/test_app.py
+++ Test/test_app.py
@@ -1,9 +1,11 @@
 import unittest
-from App.multi import multiplicacion
+from App.multi import *
 
 class testmulti(unittest.TestCase):
     def test_mul(self):
         self.assertEqual(multiplicacion(2,3),6)
+    def test_suma(self):
+        self.assertEqual(suma(2,2),4)
 
 if __name__ == "__main__":
     unittest.main()
